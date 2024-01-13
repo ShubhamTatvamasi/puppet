e5ac87fd4aef2f782d051cfa93c0b313b7065bff
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2024-01-13 12:37:22 +0530

    modified:   site.pp

--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -1,5 +1,10 @@
-file {'/tmp/hello.txt':
-  ensure  => present,
-  mode    => '0644',
-  content => 'Hello, world!',
+node default {
+  package {'nginx':
+    ensure => present,
+  }
+  file {'/tmp/hello.txt':
+    ensure  => present,
+    mode    => '0644',
+    content => 'Hello, world!\n',
+  }
 }
